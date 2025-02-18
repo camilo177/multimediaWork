9f7ebcb5091587242ac8f1e81dfd16c9efd6f8ed
camilo177 <juanc.lopezca@campusucc.edu.co>
2025-02-17 19:56:22 -0500

    final version

--- Desktop/playlist/lib/main.dart
+++ Desktop/playlist/lib/main.dart
@@ -1,14 +1,12 @@
-import 'package:clean_architecture/data/repositories/song_repository_impl.dart';
-import 'package:clean_architecture/domain/usecases/delete_song.dart';
-import 'package:clean_architecture/domain/usecases/get_all_songs.dart';
-import 'package:clean_architecture/domain/usecases/save_song.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:clean_architecture/presentation/bloc/song_bloc.dart';
 import 'package:clean_architecture/presentation/bloc/song_event.dart';
 import 'package:clean_architecture/presentation/pages/song_list_page.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:clean_architecture/injection.dart'; 
 
 void main() {
+  initializeDependencies(); 
   runApp(const MyApp());
 }
 
@@ -17,12 +15,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Create instances of dependencies
-    final songRepository = SongRepositoryImpl();
-    final getAllSongs = GetAllSongs(songRepository);
-    final saveSong = SaveSong(songRepository);
-    final deleteSong = DeleteSong(songRepository);
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Spotify UCC version',
@@ -31,11 +23,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: false,
       ),
       home: BlocProvider(
-        create: (context) => SongBloc(
-          getAllSongs: getAllSongs,
-          saveSong: saveSong,
-          deleteSong: deleteSong,
-        )..add(LoadSongs()),
+        create: (context) => getIt<SongBloc>()..add(LoadSongs()), 
         child: const SongListPage(),
       ),
     );
