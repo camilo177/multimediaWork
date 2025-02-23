6ddb9c4832f0d21fb0e49596b451f4deb6636f17
Juan Camilo López Caicedo <134346072+camilo177@users.noreply.github.com>
2025-02-22 22:31:20 -0500

    Delete Desktop directory

--- Desktop/playlist/lib/domain/repositories/song_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:clean_architecture/domain/entities/song.dart';
-
-abstract class SongRepository {
-  Future<List<Song>> getAllSongs();
-  Future<Song> getSongById(String id);
-  Future<void> saveSong(Song song);
-  Future<void> deleteSong(String id);
-}
